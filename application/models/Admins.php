6519393e815e12c9153833f0ca1586482d3420a6
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 05:15:06 +0100

    Update Admins.php

--- application/models/Admins.php
+++ application/models/Admins.php
@@ -1,52 +1,55 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Admins extends CI_Model {
 
-Class Admins extends CI_Model {
-  
-  private $id;
-  private $table_name = 'admin';
-  
-  public function __construct()
-  {
-    parent::__construct();
-  }
-  
-  public function checkLogin($login, $password)
-  {
-    $hash = hash('sha256', $password);
-    $query = $this->db->get_where('admin', array('login' => $login, 'password' => $hash));
-    return $query->num_rows() > 0 ? TRUE : FALSE;
-  }
-  
-  public function setId($id)
-  {
-    $this->id = $id;
-  }
-  
-  public function getId()
-  {
-    return $this->id;
-  }
-  
-  public function add()
-  {
-    
-  }
-  
-  public function update()
-  {
-    
-  }
-  
-  public function get()
-  {
-    
-  }
-  
-  public function remove()
-  {
-    
-  }
-  
+    private $id;
+    private $table_name = 'admin';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function checkLogin($login, $password) {
+        $hash = hash('sha256', $password);
+        $query = $this->db->get_where($this->table_name, array('login' => $login, 'password' => $hash));
+        return $query->num_rows() > 0 ? TRUE : FALSE;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function add($data) {
+        $data['password'] = hash('sha256', $data['password']);
+        return $this->db->insert($this->table_name, $data);
+    }
+
+    public function update($id, $data) {
+        if(isset($data['password'])) {
+            $data['password'] = hash('sha256', $data['password']);
+        }
+        $this->db->where('id', $id);
+        return $this->db->update($this->table_name, $data);
+    }
+
+    public function get($id = NULL) {
+        if ($id === NULL) {
+            $query = $this->db->get($this->table_name);
+            return $query->result_array();
+        } else {
+            $query = $this->db->get_where($this->table_name, array('id' => $id));
+            return $query->row_array();
+        }
+    }
+
+    public function remove($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete($this->table_name);
+    }
+
 }
