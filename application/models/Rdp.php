78b99f79a3688ff7b8a1748c6f9fcbb6680992bb
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 05:50:15 +0100

    Update Rdp.php

--- application/models/Rdp.php
+++ application/models/Rdp.php
@@ -1,53 +1,42 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Rdp extends CI_Model {
 
-Class Rdp extends CI_Model {
-  
-  private $id;
-  private $table_name = 'rdp';
-  
-  public function __construct()
-  {
-    parent::__construct();
-  }
-  
-  public function setId($id)
-  {
-    $this->id = $id;
-  }
-  
-  public function getId()
-  {
-    return $this->id;
-  }
-  
-  public function add()
-  {
-    
-  }
-  
-  public function update()
-  {
-    
-  }
-  
-  public function get()
-  {
-    
-  }
-  
-  public function remove()
-  {
-    
-  }
-  
-	public function getList()
-	{
-    $this->db->select('*');
-    $this->db->from($this->table_name);
-    $this->db->join('user', 'user.id = rdp.seller_id');
-    $query = $this->db->get();
-    return $query->result();
-	}
+    private $id;
+    private $table_name = 'rdp';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    // Existing getList() method...
+
+    public function add($data) {
+        return $this->db->insert($this->table_name, $data);
+    }
+
+    public function update($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update($this->table_name, $data);
+    }
+
+    public function get($id) {
+        $query = $this->db->get_where($this->table_name, array('id' => $id));
+        return $query->row_array();
+    }
+
+    public function remove($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete($this->table_name);
+    }
+
 }
