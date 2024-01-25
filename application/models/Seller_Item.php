7d3ebe889a975a89841f398727cd26add765e38d
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 07:38:54 +0100

    Update Seller_Item.php

--- application/models/Seller_Item.php
+++ application/models/Seller_Item.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 Class Seller_Item extends CI_Model {
   
   private $id;
@@ -22,26 +21,26 @@ Class Seller_Item extends CI_Model {
     return $this->id;
   }
   
-  public function add($name,$type,$price,$description/*,$seller_id*/)
+  public function add($name, $type, $price, $description, $seller_id)
   {
     $data = array(
       'item_name' => $name,
       'item_price' => $price,
       'item_type' => $type,
       'item_description' => $description,
-      'seller_id' => 1
+      'seller_id' => $seller_id
     );
     $this->db->insert($this->table_name, $data);
   }
   
-  public function update($id,$name,$type,$price,$description/*,$seller_id*/)
+  public function update($id, $name, $type, $price, $description, $seller_id)
   {
     $data = array(
       'item_name' => $name,
       'item_price' => $price,
       'item_type' => $type,
       'item_description' => $description,
-      'seller_id' => 1
+      'seller_id' => $seller_id
     );
     $this->db->where('id', $id);
     $this->db->update($this->table_name, $data);
@@ -49,19 +48,27 @@ Class Seller_Item extends CI_Model {
   
   public function get($id = FALSE)
   {
-    $id = $id ? $id : $this->id;
+    if ($id === FALSE) {
+      $query = $this->db->get($this->table_name);
+      return $query->result();
+    }
+
+    $this->db->where('id', $id);
+    $query = $this->db->get($this->table_name);
+    return $query->row();
   }
   
   public function remove($id)
   {
-    $this->db->delete($this->table_name, array('id' => $id));
+    $this->db->where('id', $id);
+    $this->db->delete($this->table_name);
   }
   
-  public function getList($seller_id = 1)
+  public function getList($seller_id)
   {
     $this->db->select('*');
     $this->db->from($this->table_name);
-    $this->db->where('seller_id',$seller_id);
+    $this->db->where('seller_id', $seller_id);
     $query = $this->db->get();
     return $query->result();
   }
