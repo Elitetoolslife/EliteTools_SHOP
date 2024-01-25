b82dfb423b5c5017ac1027cb7b36dac425fbb830
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 07:55:49 +0100

    Update template_helper.php

--- application/helpers/template_helper.php
+++ application/helpers/template_helper.php
@@ -1,12 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
 function loginHeader()
 {
-  
+  $CI =& get_instance();
+  $CI->load->view('login_header'); // Assuming you have a view named 'login_header.php'
 }
 
 function loginFooter()
 {
-  
+  $CI =& get_instance();
+  $CI->load->view('login_footer'); // Assuming you have a view named 'login_footer.php'
 }
