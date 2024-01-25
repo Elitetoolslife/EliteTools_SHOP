6cb81600d3fcac07f53f785fa345f7ce7c79b99f
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 07:51:35 +0100

    Update User.php

--- application/models/User.php
+++ application/models/User.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 Class User extends CI_Model {
   
   private $id;
@@ -22,24 +21,31 @@ Class User extends CI_Model {
     return $this->id;
   }
   
-  public function add()
+  public function add($data)
   {
-    
+    $this->db->insert($this->table_name, $data);
+    return $this->db->insert_id();
   }
   
-  public function update()
+  public function update($id, $data)
   {
-    
+    $this->db->where('id', $id);
+    $this->db->update($this->table_name, $data);
+    return $this->db->affected_rows();
   }
   
-  public function get()
+  public function get($id)
   {
-    
+    $this->db->where('id', $id);
+    $query = $this->db->get($this->table_name);
+    return $query->row();
   }
   
-  public function remove()
+  public function remove($id)
   {
-    
+    $this->db->where('id', $id);
+    $this->db->delete($this->table_name);
+    return $this->db->affected_rows();
   }
   
   public function checkLogin($login, $password)
@@ -49,10 +55,9 @@ Class User extends CI_Model {
     return $query->num_rows() > 0 ? TRUE : FALSE;
   }
   
-  
-	public function getList()
-	{
-		$query = $this->db->get($this->table_name);
-		return $query->result();
-	}
+  public function getList()
+  {
+    $query = $this->db->get($this->table_name);
+    return $query->result();
+  }
 }
