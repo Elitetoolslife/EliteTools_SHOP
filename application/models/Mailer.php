812fc333f21b77963a1c423ddeba014cb3338723
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 05:19:45 +0100

    Update Mailer.php

--- application/models/Mailer.php
+++ application/models/Mailer.php
@@ -1,53 +1,52 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Mailer extends CI_Model {
 
-Class Mailer extends CI_Model {
-  
-  private $id;
-  private $table_name = 'mailer';
-  
-  public function __construct()
-  {
-    parent::__construct();
-  }
-  
-  public function setId($id)
-  {
-    $this->id = $id;
-  }
-  
-  public function getId()
-  {
-    return $this->id;
-  }
-  
-  public function add()
-  {
-    
-  }
-  
-  public function update()
-  {
-    
-  }
-  
-  public function get()
-  {
-    
-  }
-  
-  public function remove()
-  {
-    
-  }
- 
-	public function getList()
-	{
-		$this->db->select('*');
-    $this->db->from($this->table_name);
-    $this->db->join('user', 'user.id = mailer.seller_id');
-    $query = $this->db->get();
-    return $query->result();
-	}
+    private $id;
+    private $table_name = 'mailer';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function add($data) {
+        return $this->db->insert($this->table_name, $data);
+    }
+
+    public function update($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update($this->table_name, $data);
+    }
+
+    public function get($id = NULL) {
+        if ($id === NULL) {
+            $query = $this->db->get($this->table_name);
+            return $query->result_array();
+        } else {
+            $query = $this->db->get_where($this->table_name, array('id' => $id));
+            return $query->row_array();
+        }
+    }
+
+    public function remove($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete($this->table_name);
+    }
+
+    public function getList() {
+        $this->db->select('*');
+        $this->db->from($this->table_name);
+        $this->db->join('user', 'user.id = mailer.seller_id');
+        $query = $this->db->get();
+        return $query->result();
+    }
 }
