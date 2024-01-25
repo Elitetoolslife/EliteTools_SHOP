6208d020f6404e05ff53ea79261d62dcb9787dc1
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-25 05:20:43 +0100

    Update Message.php

--- application/models/Message.php
+++ application/models/Message.php
@@ -1,71 +1,38 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Message extends CI_Model {
 
-Class Message extends CI_Model {
-  
-  private $id;
-  private $table_name = 'messages';
-  
-  public function __construct()
-  {
-    parent::__construct();
-  }
-  
-  public function setId($id)
-  {
-    $this->id = $id;
-  }
-  
-  public function getId()
-  {
-    return $this->id;
-  }
+    private $id;
+    private $table_name = 'messages';
 
-  public function getThread($entity_id,$entity_type)
-  {
-    $filter = array('entity_id' => $entity_id, 'entity_type' => $entity_type);
-    $user_id = 1;
-    $this->db->select('*');
-    $this->db->from($this->table_name);
-    $this->db->where($filter);
-    $this->db->order_by("date_created", "asc");
-    $query = $this->db->get();
-    return $query->result();
-  }
-  
-  public function add($entity_id,$entity_type,$message)
-  {
-    $data = array(
-        'entity_id' => $entity_id,
-        'entity_type' => $entity_type,
-        'message' => $message,
-        'date_created'=>date('Y-m-d h:i:s'),
-        'user_id' => 1
-    );
-    $this->db->insert($this->table_name, $data);
-    $message_id = $this->db->insert_id();
+    public function __construct() {
+        parent::__construct();
+    }
 
-    $data = array(
-        'last_reply' => $message_id,
-    );
-    $this->db->where('id', $entity_id);
-    $this->db->update($entity_type, $data);
-  }
-  
-  public function update()
-  {
-    
-  }
-  
-  public function get()
-  {
-    
-  }
-  
-  public function remove()
-  {
-    
-  }
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    // Existing getThread() and add() methods...
+
+    public function update($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update($this->table_name, $data);
+    }
+
+    public function get($id) {
+        $query = $this->db->get_where($this->table_name, array('id' => $id));
+        return $query->row_array();
+    }
+
+    public function remove($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete($this->table_name);
+    }
 
 }
